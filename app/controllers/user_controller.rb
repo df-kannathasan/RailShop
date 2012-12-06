b59f032e7126cc1f1f342fc2cdd6400759c30d28
Kannathasan <kannathasan@railsfactory.org>
2012-12-06 15:18:09 +0530

    second commit

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
+  include ApplicationHelper
   before_filter :protect, :only => :index
   skip_before_filter :verify_authenticity_token  
   def index
@@ -7,53 +8,63 @@ class UserController < ApplicationController
   
   def register
   @title = "Register"
-  if request.post?
+  if param_posted?(:user)
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = @user.id
+      @user.login!(session)
       flash[:notice] = "User #{@user.screen_name} created!"
-      redirect_to :action => "index"
+      redirect_to_forwarding_url
+    else
+      @user.clear_password!
     end
   end
-  end
+end
+
 def login
 @title = "Log in to RailsSpace"
-if request.post? and params[:user]
+if param_posted?(:user)
   @user = User.new(params[:user])
   user = User.find_by_screen_name_and_password(@user.screen_name,@user.password)
   if user
-    session[:user_id] = user.id
-    flash[:notice] = "User #{user.screen_name} logged in!"
-    if (redirect_url = session[:protected_page])
-    session[:protected_page] = nil
-    redirect_to redirect_url
-    else
-    redirect_to :action => "index"
+    user.login!(session)
+    if @user.remember_me == "1"
+      cookies[:remember_me] = { :value   => "1", :expires => 10.years.from_now }
     end
-
+    flash[:notice] = "User #{user.screen_name} logged in!"
+    redirect_to_forwarding_url
   else
-    # Don't show the password in the view.
-    @user.password = nil
+    @user.clear_password!
     flash[:notice] = "Invalid screen name/password combination"
   end
 end
 end
 def logout
-  session[:user_id] = nil
+  User.logout!(session)
   flash[:notice] = "Logged out"
   redirect_to :action => "index", :controller => "site"
 end
 
 private
+def param_posted?(symbol)
+  request.post? and params[symbol]
+end
+
+def redirect_to_forwarding_url
+  if (redirect_url = session[:protected_page])
+    session[:protected_page] = nil
+    redirect_to redirect_url
+  else
+    redirect_to :action => "index"
+  end
+end
+
   def protect
-  unless session[:user_id]
-  session[:protected_page] = request.request_uri
+  unless logged_in?
+    session[:protected_page] = request.request_uri
     flash[:notice] = "Please log in first"
     redirect_to :action => "login"
    return false
+   end
  end
   # This will be a protected page for viewing user information.
-end
-
-
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,9 +6,11 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_rails_space_session',
-  :secret      => '9e840028e12e5d47873514579a7c7d5c2cf5af7e2efdfb6dfa1779ca14e7f2b2357d3986d87cc330290c1190dccd1aa2dc023d98b5c75562cdbe0c3d2a910cf4'
+  :secret      => '9e840028e12e5d47873514579a7c7d5c2cf5af7e2efdfb6dfa1779ca14e7f2b2357d3986d87cc330290c1190dccd1aa2dc023d98b5c75562cdbe0c3d2a910cf4',
+  :expire_after =>1.minutes
 }
-
+#ActionController::Base.session_options[:secure] = true
+#ActionController::Base.session_options[:expire_after]=1.minutes
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
